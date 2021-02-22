4245bbe0eba665cc0007a7111c898a1af0c2d1cd
ADITYA PATIL <adityaypatil99@gmail.com>
2021-02-22 11:01:02 +0530

    first commit

---
+++ exp2.r
@@ -0,0 +1,45 @@
+#UNDERSTANDING THE DATATYPES
+#numeric
+x<-10.5
+print(class(x))
+
+#integer
+x<-1000L
+print(class(x))
+
+
+#character
+v <- "ADITYA"
+print(class(v))
+
+#logical
+v <- TRUE 
+print(class(v))
+
+#complex
+v <- 2+5i
+print(class(v))
+
+# Logical 
+print(class(TRUE)) 
+
+# Integer 
+print(class(3L)) 
+
+# Numeric 
+print(class(10.5)) 
+
+# Complex 
+print(class(1+2i)) 
+
+# Character 
+print(class("12-04-2020")) 
+
+as.numeric(x)
+
+as.integer(x)
+
+as.character(x)
+
+as.logical(x)
+
